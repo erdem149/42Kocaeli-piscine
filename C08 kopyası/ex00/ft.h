d3c128a5018a15af3b20f2e8a2203710f49281b9
Erdem Yilmaz <eryilmaz@k1m07s06.42kocaeli.com.tr>
2021-10-21 18:50:43 +0300

    42 de sınavdan bir gün önceki gönderim

---
+++ C08 kopyası/ex00/ft.h
@@ -0,0 +1,52 @@
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
